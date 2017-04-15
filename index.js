cef94157a5b2f5b094bccc6fda5a29771251d945
Matthew Jesuele <matt@makeapps.io>
2017-04-15 15:14:19 -0700

    Okay now there's a restify server actually serving up the bot endpoint. And there is ESLint too.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('running!!!');
+const restify = require(`restify`);
+const builder = require(`botbuilder`);
+
+// =========================================================
+// Bot Setup
+// =========================================================
+
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+   console.log(`%s listening to %s`, server.name, server.url);
+});
+
+// Create chat bot
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD,
+});
+const bot = new builder.UniversalBot(connector);
+server.post(`/api/messages`, connector.listen());
+
+// =========================================================
+// Bots Dialogs
+// =========================================================
+
+bot.dialog(`/`, session => {
+    session.send(`Hello World`);
+});
